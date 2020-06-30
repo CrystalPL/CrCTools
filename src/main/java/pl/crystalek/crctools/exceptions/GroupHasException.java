8c5eee23c06d985f54be8259bda8ddf445b422a1
Crystal <janwolowski@crystalek.pl>
2020-06-30 10:48:21 +0200

    add permission system

---
+++ src/main/java/pl/crystalek/crctools/exceptions/GroupHasException.java
@@ -0,0 +1,7 @@
+package pl.crystalek.crctools.exceptions;
+
+public class GroupHasException extends Exception {
+    public GroupHasException(final String message) {
+        super(message);
+    }
+}
